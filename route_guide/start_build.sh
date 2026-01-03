f411fc9a2af5e8fdae661fb3d15539bbf30994ec
Ubuntu <ubuntu@localhost.localdomain>
2026-01-03 14:06:40 +0800

    调整脚本

--- route_guide/start_build.sh
+++ route_guide/start_build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 mkdir -p build && cd build
-cmake .. -DCMAKE_C_COMPILER=/usr/local/bin/gcc -DCMAKE_CXX_COMPILER=/usr/local/bin/g++
+# cmake .. -DCMAKE_C_COMPILER=/usr/local/bin/gcc -DCMAKE_CXX_COMPILER=/usr/local/bin/g++
+cmake ..
 make -j$(nproc)
